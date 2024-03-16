b01c6ea9c657a9f5ec1ac9d472c98523a3bf6f87
sireesha86 <154280890+sireesha86@users.noreply.github.com>
2024-03-16 00:28:34 +0000

    added defensive programming

--- temp_convertor.py
+++ temp_convertor.py
@@ -1,4 +1,8 @@
 temp = int(input('Enter the temperature in Degree Celsius:'))
-def degree_to_celsius(temp):
+try:
+ def degree_to_celsius(temp):
   return temp + 273.15
-print(f'Your temperature in kelvin is: {degree_to_celsius(temp)}')
+ print(f'Your temperature in kelvin is: {degree_to_celsius(temp)}')
+
+except Exception as e:
+  print(e)
